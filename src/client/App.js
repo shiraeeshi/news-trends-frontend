4676713df51fe22925af5550a54089657ca53269
shiraeeshi <shiraeeshi@mail.ru>
2018-08-16 11:58:33 +0600

    show news

--- src/client/App.js
+++ src/client/App.js
@@ -5,24 +5,69 @@ import ReactImage from "./react.png";
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: null };
+    this.state = { news: null };
   }
 
   componentDidMount() {
-    fetch("/api/getUsername")
+    fetch("/api/news")
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+      .then(news => this.setState({ news }));
+  }
+
+  toFormattedDate(unixTimestamp) {
+    const dt = new Date(unixTimestamp * 1);
+    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    const year = dt.getFullYear();
+    const month = months[dt.getMonth()];
+    const date = "0" + dt.getDate();
+    const hour = "0" + dt.getHours();
+    const min = "0" + dt.getMinutes();
+    const sec = "0" + dt.getSeconds();
+    const time = date.substr(-2) + ' ' + month + ' ' + year + ' ' + hour.substr(-2) + ':' + min.substr(-2) + ':' + sec.substr(-2) ;
+    return time;
   }
 
   render() {
     return (
       <div>
-        {this.state.username ? (
-          <h1>Hello {this.state.username}</h1>
+        <img src={ReactImage} width="30%" alt="react" />
+        {this.state.news && this.state.news.length > 0 ? (
+          <div>
+          {this.state.news.map(e =>
+            (
+              <div>
+                <h2>
+                  {e.title}
+                </h2>
+                <div>
+                  {this.toFormattedDate(e.publishedAt)}
+                </div>
+                <div>
+                  <a href={e.link}>
+                    {e.link}
+                  </a>
+                </div>
+                <div>
+                  {e.tags ? (
+                    <div>
+                      tags:
+                      {e.tags.map(tag =>
+                        <span> {tag} </span>
+                      )}
+                    </div>
+                   ) : (
+                    <span>No tags</span>
+                  )}
+                </div>
+                <br/>
+              </div>
+            )
+          )
+          }
+          </div>
         ) : (
-          <h1>Loading.. please wait!</h1>
+          <h1>No news</h1>
         )}
-        <img src={ReactImage} alt="react" />
       </div>
     );
   }
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const os = require('os');
+const fetch = require('node-fetch');
 
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/news', (request, response) =>
+  fetch('http://rest-api:9090/news')
+    .then(res => res.json())
+    .then(news => response.send(news))
+);
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
